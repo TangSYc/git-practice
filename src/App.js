15c16d69de9f90adb15220e3b6f2ce0bfe1e0030
xurui07 <xurui07@meituan.com>
2021-10-13 14:20:32 +0800

    fix: lint

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>npm-test</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          软件服务组react项目
+        </a>
+        <div>table</div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
